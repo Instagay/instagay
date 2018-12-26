2789262b3e56fadf9b200d823845dd66f0d38026
Dan Taeyoung <you@example.com>
2018-12-26 01:01:42 -0500

    scraping experiments going on

---
+++ dev/3_pupp_instagram_Screenshot.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+(async() => { 
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+  const page = await browser.newPage();
+
+  await page.goto('https://www.instagram.com/explore/tags/sunnyxmas/');
+// await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.3.1.slim.min.js'});
+
+  const viewPortHeight = await page.viewport().height
+
+  let previousHeight = await page.evaluate('document.body.scrollHeight');
+  await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+  await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+  await page.waitFor(500)
+
+  await page.evaluate(() => {
+   var mostrecentdiv = document.evaluate("//h2[contains(., 'Most recent')]", document, null, XPathResult.ANY_TYPE, null ).iterateNext().nextSibling;
+  mostrecentdiv.style.backgroundColor = "blue";
+  });
+
+  await page.screenshot({path: 'example.png', fullPage: true});
+
+  await browser.close();
+
+})();
